d7ede3c3b8222980802bb211bcbbb28ac2c42a7b
lindbergpg <Lindbergpg@gmail.com>
2022-09-22 16:39:14 -0300

    Finalizado

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const form = document.getElementById('form-contatos');
 const imgAdicionado = '<img src="./imagens/Ok.png" alt="Ok" />';
+const contato = [];
+const tel = [];
 
 let linhas = '';
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+    atualizaQuantContatos();
+});
+
+function adicionaLinha() {
     const inputNomeContato = document.getElementById('nome-contato');
     const inputNumeroTelefone = document.getElementById('numero-telefone');
+
+    if (tel.includes(inputNumeroTelefone.value)) {
+        alert(`O número de telefone ${inputNumeroTelefone.value} já está adicionado.`)
+    } else {
+
+    contato.push(inputNomeContato.value);
+    tel.push(inputNumeroTelefone.value);
 
     let linha = '<tr>';
     linha += `<td>${inputNomeContato.value}</td>`;
@@ -16,10 +31,25 @@ form.addEventListener('submit', function(e) {
     linha += '</tr>';
 
     linhas += linha;
-
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
+    }
 
     inputNomeContato.value = '';
     inputNumeroTelefone.value = '';
-});
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atualizaQuantContatos() {
+    const quantTotal = calculaTotal();
+
+    document.getElementById('contador').innerHTML = quantTotal
+}
+
+function calculaTotal() {
+    const somaContatos = contato.length
+
+    return somaContatos;
+}
